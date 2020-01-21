4129c341b939e0df8de1d84bb0a0ecab66d972e7
Peter Parisi <pparisi@us-pparisi.ad.optimizely.net>
2020-01-21 12:18:38 -0800

    fixed messaging

--- public/js/index.js
+++ public/js/index.js
@@ -103,7 +103,7 @@ function calculateCommission () {
 };
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return (x.toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function changeText () {
